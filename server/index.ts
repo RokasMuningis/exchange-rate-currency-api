2ba4eaa1a28eaa2c33dd6ddfecc45d57fa88e66e
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2020-10-24 16:12:32 +0300

    feat(server): Basic server listener

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,9 @@
-import https from "https";
+import { app, get } from "./lib/routes";
+import dotenv from "dotenv";
+import { createServer } from "./lib/server";
+
+dotenv.config();
+
+(async () => {
+    const server = await createServer();
+})();
--- server/lib/routes.ts
+++ server/lib/routes.ts
@@ -3,27 +3,39 @@ import type { IncomingMessage, ServerResponse } from "http";
 type Callback = (req: IncomingMessage, res: ServerResponse) => void;
 type Route = [pattern: RegExp, callback: Callback];
 type Routes = Array<Route>;
-enum Methods {
+export enum Methods {
     GET = "GET",
     POST = "POST",
     PUT = "PUT",
 }
 type RoutesByMethod = Record<Methods, Routes>;
 
-const App: RoutesByMethod = {
+const _app: RoutesByMethod = {
     [Methods.GET]: [],
     [Methods.POST]: [],
     [Methods.PUT]: [],
 }
 
 export const get = (pattern: string, callback: Callback) => {
-    App.GET = [...App.GET, [RegExp(pattern), callback]];
+    _app.GET = [..._app.GET, [RegExp(pattern), callback]];
 }
 export const post = (pattern: string, callback: Callback) => {
-    App.POST = [...App.POST, [RegExp(pattern), callback]];
+    _app.POST = [..._app.POST, [RegExp(pattern), callback]];
 }
 export const put = (pattern: string, callback: Callback) => {
-    App.PUT = [...App.PUT, [RegExp(pattern), callback]];
+    _app.PUT = [..._app.PUT, [RegExp(pattern), callback]];
+}
+
+export const app = {
+    get [Methods.GET] () {
+        return _app.GET;
+    },
+    get [Methods.POST] () {
+        return _app.POST;
+    },
+    get [Methods.PUT] () {
+        return _app.PUT;
+    },
 }
 
 
