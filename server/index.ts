51296f5ff9a38e7e82c688ffd7184ec7d2009291
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2020-10-24 16:15:26 +0300

    feat(server): add basic / route for health check

--- server/index.ts
+++ server/index.ts
@@ -6,4 +6,9 @@ dotenv.config();
 
 (async () => {
     const server = await createServer();
+    server.on.get("/", (_, res) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Hello world!");
+        return;
+    });
 })();
